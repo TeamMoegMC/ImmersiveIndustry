d7b761e1fadff660660771472183f04b20e02cc8
yuesha-yc <kevinwangyichen@gmail.com>
2021-10-28 00:26:07 -0700

    0.1.0

--- src/main/java/com/teammoeg/immersiveindustry/content/electrolyzer/ElectrolyzerTileEntity.java
+++ src/main/java/com/teammoeg/immersiveindustry/content/electrolyzer/ElectrolyzerTileEntity.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2021 TeamMoeg
+ *
+ * This file is part of Immersive Industry.
+ *
+ * Immersive Industry is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Immersive Industry is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Immersive Industry. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.teammoeg.immersiveindustry.content.electrolyzer;
 
 import blusunrize.immersiveengineering.api.IEEnums;
--- src/main/java/com/teammoeg/immersiveindustry/content/steamturbine/SteamTurbineMultiblock.java
+++ src/main/java/com/teammoeg/immersiveindustry/content/steamturbine/SteamTurbineMultiblock.java
@@ -1,19 +1,19 @@
 /*
  * Copyright (c) 2021 TeamMoeg
  *
- * This file is part of Frosted Heart.
+ * This file is part of Immersive Industry.
  *
- * Frosted Heart is free software: you can redistribute it and/or modify
+ * Immersive Industry is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3.
  *
- * Frosted Heart is distributed in the hope that it will be useful,
+ * Immersive Industry is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Frosted Heart. If not, see <https://www.gnu.org/licenses/>.
+ * along with Immersive Industry. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.teammoeg.immersiveindustry.content.steamturbine;
