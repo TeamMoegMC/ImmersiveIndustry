20600b7ca14515f60d1696a3bd473462dd1065ab
dashuaibia <936336596@qq.com>
2021-11-14 12:43:48 +0800

    1.3

--- src/main/java/com/teammoeg/immersiveindustry/content/steamturbine/SteamTurbineBlock.java
+++ src/main/java/com/teammoeg/immersiveindustry/content/steamturbine/SteamTurbineBlock.java
@@ -30,7 +30,7 @@ public class SteamTurbineBlock<T extends MultiblockPartTileEntity<? super T>> ex
 
     public SteamTurbineBlock(String name, RegistryObject type) {
         super(name, Properties.create(Material.IRON).hardnessAndResistance(4.0F, 40.0F).notSolid(), type);
-    }
+        }
 
     @Override
     public boolean hasTileEntity(BlockState state) {
