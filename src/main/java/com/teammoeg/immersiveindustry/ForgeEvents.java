2804aab8f45276e5a221232e52e382cbb26c8a30
xkball <45330674+xkball@users.noreply.github.com>
2025-04-26 00:06:01 +0800

    fix mixin conflict on MixinMultiBlockItem

--- src/main/java/com/teammoeg/immersiveindustry/ForgeEvents.java
+++ src/main/java/com/teammoeg/immersiveindustry/ForgeEvents.java
@@ -18,6 +18,15 @@
 
 package com.teammoeg.immersiveindustry;
 
+import com.teammoeg.immersiveindustry.mixin.MixinMultiblockPartBlockAccess;
+import net.minecraft.world.Clearable;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = IIMain.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
@@ -43,4 +52,36 @@ public class ForgeEvents {
 //            System.out.println(list);
 //        }
 //    }
+    
+    @SubscribeEvent
+    public static void onItemUse(PlayerInteractEvent.RightClickBlock event) {
+        var item = event.getItemStack();
+        if(!(item.getItem() instanceof BlockItem blockItem)) return;
+        if(blockItem.getBlock() instanceof MixinMultiblockPartBlockAccess block){
+            event.setCanceled(true);
+            event.setCancellationResult(InteractionResult.SUCCESS);
+            var level = event.getLevel();
+            if(level.isClientSide()) return;
+            var hitResult = event.getHitVec();
+            var multiblock = block.getMultiblock();
+            var structure = multiblock.getStructure().apply(level);
+            for(var aBlock : structure){
+                var pos = aBlock.pos().offset(hitResult.getBlockPos());
+                if (aBlock.nbt() != null) {
+                    BlockEntity blockentity = level.getBlockEntity(pos);
+                    Clearable.tryClear(blockentity);
+                    level.setBlock(pos, Blocks.BARRIER.defaultBlockState(), 20);
+                }
+                if (level.setBlock(pos,aBlock.state(), Block.UPDATE_ALL)) {
+                    if (aBlock.nbt() != null) {
+                        BlockEntity be = level.getBlockEntity(pos);
+                        if (be != null) {
+                            be.load(aBlock.nbt());
+                            be.setChanged();
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
